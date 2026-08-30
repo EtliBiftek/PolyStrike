44501171690ddde167e69c4f269f2355533f8879
Pifo <97687807+EtliBiftek@users.noreply.github.com>
2026-08-31 01:52:23 +0300

    Show utility state in prototype HUD

--- Assets/Scripts/Core/DebugHud.cs
+++ Assets/Scripts/Core/DebugHud.cs
@@ -9,6 +9,7 @@ namespace PolyStrike.Core
         private Health health;
         private HitscanWeapon weapon;
         private PlayerMovement movement;
+        private UtilityController utility;
         private GUIStyle textStyle;
         private GUIStyle crosshairStyle;
 
@@ -16,6 +17,7 @@ namespace PolyStrike.Core
         {
             health = GetComponent<Health>();
             movement = GetComponent<PlayerMovement>();
+            utility = GetComponent<UtilityController>();
             weapon = GetComponentInChildren<HitscanWeapon>();
         }
 
@@ -24,7 +26,7 @@ namespace PolyStrike.Core
             EnsureStyles();
 
             GUI.Label(new Rect(20f, 20f, 600f, 30f), Localization.Get("prototype.title"), textStyle);
-            GUI.Label(new Rect(20f, 48f, 1100f, 30f), Localization.Get("prototype.controls"), textStyle);
+            GUI.Label(new Rect(20f, 48f, 1400f, 30f), Localization.Get("prototype.controls"), textStyle);
 
             if (movement != null)
             {
@@ -44,23 +46,68 @@ namespace PolyStrike.Core
                 GUI.Label(new Rect(180f, Screen.height - 55f, 220f, 30f), armorText, textStyle);
             }
 
-            if (weapon != null)
-            {
-                GUI.Label(new Rect(Screen.width - 300f, Screen.height - 115f, 280f, 30f), weapon.DisplayName, textStyle);
-
-                var accuracyText = string.Format(Localization.Get("hud.inaccuracy"), weapon.CurrentInaccuracy);
-                GUI.Label(new Rect(Screen.width - 300f, Screen.height - 85f, 280f, 30f), accuracyText, textStyle);
-
-                var ammoText = string.Format(Localization.Get("hud.ammo"), weapon.AmmoInMagazine, weapon.ReserveAmmo);
-                GUI.Label(new Rect(Screen.width - 300f, Screen.height - 55f, 280f, 30f), ammoText, textStyle);
-
-                if (weapon.IsReloading)
-                    GUI.Label(new Rect(Screen.width - 300f, Screen.height - 145f, 280f, 30f), Localization.Get("hud.reloading"), textStyle);
-                else if (weapon.IsDeploying)
-                    GUI.Label(new Rect(Screen.width - 300f, Screen.height - 145f, 280f, 30f), Localization.Get("hud.deploying"), textStyle);
-            }
+            if (utility != null && utility.IsEquipped)
+                DrawUtility();
+            else if (weapon != null)
+                DrawWeapon();
 
             GUI.Label(new Rect(Screen.width * 0.5f - 15f, Screen.height * 0.5f - 18f, 30f, 36f), "+", crosshairStyle);
+        }
+
+        private void DrawWeapon()
+        {
+            GUI.Label(new Rect(Screen.width - 300f, Screen.height - 115f, 280f, 30f), weapon.DisplayName, textStyle);
+
+            var accuracyText = string.Format(Localization.Get("hud.inaccuracy"), weapon.CurrentInaccuracy);
+            GUI.Label(new Rect(Screen.width - 300f, Screen.height - 85f, 280f, 30f), accuracyText, textStyle);
+
+            var ammoText = string.Format(Localization.Get("hud.ammo"), weapon.AmmoInMagazine, weapon.ReserveAmmo);
+            GUI.Label(new Rect(Screen.width - 300f, Screen.height - 55f, 280f, 30f), ammoText, textStyle);
+
+            if (weapon.IsReloading)
+                GUI.Label(new Rect(Screen.width - 300f, Screen.height - 145f, 280f, 30f), Localization.Get("hud.reloading"), textStyle);
+            else if (weapon.IsDeploying)
+                GUI.Label(new Rect(Screen.width - 300f, Screen.height - 145f, 280f, 30f), Localization.Get("hud.deploying"), textStyle);
+        }
+
+        private void DrawUtility()
+        {
+            var grenadeName = Localization.Get(GetGrenadeKey(utility.SelectedType));
+            var utilityText = string.Format(Localization.Get("hud.utility"), grenadeName, utility.SelectedCount);
+            GUI.Label(new Rect(Screen.width - 330f, Screen.height - 85f, 310f, 30f), utilityText, textStyle);
+
+            if (!utility.IsPrimed)
+                return;
+
+            var throwName = Localization.Get(GetThrowKey(utility.ThrowStrength));
+            var throwText = string.Format(Localization.Get("hud.utility_throw"), throwName);
+            GUI.Label(new Rect(Screen.width - 330f, Screen.height - 55f, 310f, 30f), throwText, textStyle);
+        }
+
+        private static string GetGrenadeKey(GrenadeType type)
+        {
+            switch (type)
+            {
+                case GrenadeType.HighExplosive:
+                    return "grenade.he";
+                case GrenadeType.Flashbang:
+                    return "grenade.flash";
+                case GrenadeType.Smoke:
+                    return "grenade.smoke";
+                case GrenadeType.Molotov:
+                    return "grenade.molotov";
+                default:
+                    return "grenade.he";
+            }
+        }
+
+        private static string GetThrowKey(float strength)
+        {
+            if (strength <= 0.1f)
+                return "grenade.throw.short";
+            if (strength < 0.9f)
+                return "grenade.throw.medium";
+            return "grenade.throw.full";
         }
 
         private void EnsureStyles()
