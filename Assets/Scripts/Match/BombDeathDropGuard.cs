f084ef5203c544feb5db9d521df9fd724146d200
Pifo <97687807+EtliBiftek@users.noreply.github.com>
2026-08-31 02:57:52 +0300

    Remove bomb drop installer indirection

--- Assets/Scripts/Match/BombDeathDropGuard.cs
+++ Assets/Scripts/Match/BombDeathDropGuard.cs
@@ -3,23 +3,15 @@ using UnityEngine;
 
 namespace PolyStrike.Match
 {
+    [RequireComponent(typeof(MatchParticipant))]
     public sealed class BombDeathDropGuard : MonoBehaviour
     {
         private MatchParticipant participant;
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        private static void CreateInstaller()
-        {
-            var root = new GameObject("Bomb Drop Installer");
-            Object.DontDestroyOnLoad(root);
-            root.AddComponent<BombDropInstaller>();
-        }
-
         private void Awake()
         {
             participant = GetComponent<MatchParticipant>();
-            if (participant != null)
-                participant.Died += OnParticipantDied;
+            participant.Died += OnParticipantDied;
         }
 
         private void OnDestroy()
@@ -36,22 +28,6 @@ namespace PolyStrike.Match
             participant.GiveBomb(false);
             var origin = transform.position + Vector3.up * 0.55f;
             DroppedMatchItem.SpawnBomb(origin, Vector3.up * SourceUnit.ToMeters(55f));
-        }
-    }
-
-    public sealed class BombDropInstaller : MonoBehaviour
-    {
-        private void Start()
-        {
-            var participants = Object.FindObjectsByType<MatchParticipant>(FindObjectsSortMode.None);
-            for (var i = 0; i < participants.Length; i++)
-            {
-                var participant = participants[i];
-                if (participant != null && participant.GetComponent<BombDeathDropGuard>() == null)
-                    participant.gameObject.AddComponent<BombDeathDropGuard>();
-            }
-
-            Destroy(gameObject);
         }
     }
 }
