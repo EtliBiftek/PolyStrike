dd3d7d2ade429a22377296aa6b0f7a312fde9c9f
Pifo <97687807+EtliBiftek@users.noreply.github.com>
2026-08-31 03:22:57 +0300

    Carry subtick action timing with player input

--- Assets/Scripts/Networking/NetworkPlayerInput.cs
+++ Assets/Scripts/Networking/NetworkPlayerInput.cs
@@ -7,11 +7,23 @@ namespace PolyStrike.Networking
     {
         [GhostField(Quantization = 1000)] public float2 Move;
         [GhostField(Quantization = 100)] public float2 Look;
+
         [GhostField] public InputEvent Jump;
+        [GhostField] public InputEvent FirePressed;
+        [GhostField] public InputEvent FireReleased;
+        [GhostField] public InputEvent SecondaryFirePressed;
+        [GhostField] public InputEvent SecondaryFireReleased;
         [GhostField] public InputEvent Reload;
+        [GhostField] public InputEvent Drop;
+
+        [GhostField] public byte JumpSubtick;
+        [GhostField] public byte FireSubtick;
+        [GhostField] public byte SecondaryFireSubtick;
         [GhostField] public byte CrouchHeld;
+        [GhostField] public byte WalkHeld;
         [GhostField] public byte FireHeld;
         [GhostField] public byte SecondaryFireHeld;
+        [GhostField] public byte UseHeld;
         [GhostField] public byte WeaponSlot;
     }
 }
