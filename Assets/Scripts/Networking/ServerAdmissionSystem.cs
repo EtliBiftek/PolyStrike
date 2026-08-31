5ba6bfa7e663b330034fb6b0e5f68b429bd8e41b
Pifo <97687807+EtliBiftek@users.noreply.github.com>
2026-08-31 03:14:10 +0300

    Enforce ten-player server admission limit

---
+++ Assets/Scripts/Networking/ServerAdmissionSystem.cs
@@ -0,0 +1,45 @@
+using Unity.Collections;
+using Unity.Entities;
+using Unity.NetCode;
+
+namespace PolyStrike.Networking
+{
+    public struct PolyStrikeConnectionAccepted : IComponentData
+    {
+    }
+
+    [WorldSystemFilter(WorldSystemFilterFlags.ServerSimulation)]
+    [UpdateInGroup(typeof(ServerSimulationSystemGroup))]
+    public partial struct ServerAdmissionSystem : ISystem
+    {
+        public void OnUpdate(ref SystemState state)
+        {
+            var acceptedCount = 0;
+            foreach (var (_, entity) in SystemAPI.Query<RefRO<NetworkStreamConnection>>().WithEntityAccess())
+            {
+                if (SystemAPI.HasComponent<PolyStrikeConnectionAccepted>(entity))
+                    acceptedCount++;
+            }
+
+            var commandBuffer = new EntityCommandBuffer(Allocator.Temp);
+            foreach (var (_, entity) in SystemAPI.Query<RefRO<NetworkStreamConnection>>().WithEntityAccess())
+            {
+                if (SystemAPI.HasComponent<PolyStrikeConnectionAccepted>(entity) ||
+                    SystemAPI.HasComponent<NetworkStreamRequestDisconnect>(entity))
+                    continue;
+
+                if (acceptedCount >= PolyStrikeNetcodeBootstrap.MaximumPlayers)
+                {
+                    commandBuffer.AddComponent<NetworkStreamRequestDisconnect>(entity);
+                    continue;
+                }
+
+                commandBuffer.AddComponent<PolyStrikeConnectionAccepted>(entity);
+                acceptedCount++;
+            }
+
+            commandBuffer.Playback(state.EntityManager);
+            commandBuffer.Dispose();
+        }
+    }
+}
